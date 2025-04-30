c876424eef16cefa721d1a0c6367f3c7470383ae
Austin Dworaczyk Wiltshire <561689+adworacz@users.noreply.github.com>
2025-04-29 19:40:42 -0700

    Fully working ReadScenes function.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # zscene
+
+## ReadScene
+```python
+core.zscene.ReadScenes(path=string)
+```
+
+Reads scene data from a given file. Adds the pertinent `_SceneChangePrev` and `_SceneChangeNext` properties
+to each frame.
+
+Currently only supports the JSON file emitted from https://github.com/rust-av/av-scenechange, although there's 
+no reason other scene file formats can't be added. Feel free to open an issue to request one.
+
+## Scene File Generation
+Scene data can be generated using something like the following:
+
+```sh
+vspipe -c y4m source.vpy - | av-scenechange -o scenes.json -
+```
+
+Note that `av-scenechange` can be built with Vapoursynth and FFMPEG support built in, 
+so there are a variety of ways that it can be fed with video data. Choose what works best
+for you.
--- src/read.zig
+++ src/read.zig
@@ -16,13 +16,14 @@ const st = vs.SampleType;
 // specifically the filter data between the Create and GetFrame functions.
 const allocator = std.heap.c_allocator;
 
+const FramesSet = std.array_hash_map.AutoArrayHashMapUnmanaged(u32, void);
+
 const ReadScenesData = struct {
     // The clip on which we are operating.
     node: ?*vs.Node,
     vi: *const vs.VideoInfo,
 
-    // The modes for each plane we will process.
-    modes: [3]u5,
+    frames_set: FramesSet,
 };
 
 fn getFrame(n: c_int, activation_reason: ar, instance_data: ?*anyopaque, frame_data: ?*?*anyopaque, frame_ctx: ?*vs.FrameContext, core: ?*vs.Core, vsapi: ?*const vs.API) callconv(.C) ?*const vs.Frame {
@@ -37,21 +38,11 @@ fn getFrame(n: c_int, activation_reason: ar, instance_data: ?*anyopaque, frame_d
         const src_frame = vsapi.?.getFrameFilter.?(n, d.node, frame_ctx);
         defer vsapi.?.freeFrame.?(src_frame);
 
-        const process = [_]bool{
-            d.modes[0] > 0,
-            d.modes[1] > 0,
-            d.modes[2] > 0,
-        };
-        // const dst = vscmn.newVideoFrame(&process, src_frame, d.vi, core, vsapi);
+        const dst = vsapi.?.copyFrame.?(src_frame, core);
+        const props = vsapi.?.getFramePropertiesRW.?(dst);
 
-        var plane_src = [_]?*const vs.Frame{
-            if (process[0]) null else src_frame,
-            if (process[1]) null else src_frame,
-            if (process[2]) null else src_frame,
-        };
-        const planes = [_]c_int{ 0, 1, 2 };
-
-        const dst = vsapi.?.newVideoFrame2.?(&d.vi.format, d.vi.width, d.vi.height, @ptrCast(&plane_src), @ptrCast(&planes), src_frame, core);
+        _ = vsapi.?.mapSetInt.?(props, "_SceneChangePrev", @intFromBool(d.frames_set.contains(@intCast(n))), vs.MapAppendMode.Replace);
+        _ = vsapi.?.mapSetInt.?(props, "_SceneChangeNext", @intFromBool(d.frames_set.contains(@intCast(n+1))), vs.MapAppendMode.Replace);
 
         return dst;
     }
@@ -63,8 +54,18 @@ export fn readScenesFree(instance_data: ?*anyopaque, core: ?*vs.Core, vsapi: ?*c
     _ = core;
     const d: *ReadScenesData = @ptrCast(@alignCast(instance_data));
     vsapi.?.freeNode.?(d.node);
+
+    d.frames_set.deinit(allocator);
+    d.frames_set = undefined;
+
     allocator.destroy(d);
 }
+
+const ScenesJson = struct{
+    scene_changes: []u32,
+    frame_count: u32,
+    speed: f32,
+};
 
 export fn readScenesCreate(in: ?*const vs.Map, out: ?*vs.Map, user_data: ?*anyopaque, core: ?*vs.Core, vsapi: ?*const vs.API) callconv(.C) void {
     _ = user_data;
@@ -76,6 +77,41 @@ export fn readScenesCreate(in: ?*const vs.Map, out: ?*vs.Map, user_data: ?*anyop
     d.node = vsapi.?.mapGetNode.?(in, "clip", 0, &err).?;
     d.vi = vsapi.?.getVideoInfo.?(d.node);
 
+    const path_len: usize = @intCast(vsapi.?.mapGetDataSize.?(in, "path", 0, &err));
+    const path = vsapi.?.mapGetData.?(in, "path", 0, &err)[0..path_len];
+
+    const file = std.fs.cwd().openFile(path, .{ .mode = .read_only }) catch {
+        vsapi.?.mapSetError.?(out, "ReadScenes: Unable to read the provided scene file.");
+        vsapi.?.freeNode.?(d.node);
+        return;
+    };
+    defer file.close();
+
+    const reader = file.reader();
+
+    var json_reader = std.json.reader(allocator, reader);
+    defer json_reader.deinit();
+    
+    const json = std.json.parseFromTokenSource(ScenesJson, allocator, &json_reader, .{}) catch {
+        vsapi.?.mapSetError.?(out, "ReadScenes: Unable to parse the scene json data.");
+        vsapi.?.freeNode.?(d.node);
+        return;
+    };
+    defer json.deinit();
+
+    const scenes = json.value.scene_changes;
+
+    d.frames_set = FramesSet{};
+    d.frames_set.ensureTotalCapacity(allocator, scenes.len) catch {
+        vsapi.?.mapSetError.?(out, "ReadScenes: Unable to allocate space for frame set.");
+        vsapi.?.freeNode.?(d.node);
+        return;
+    };
+
+    for (scenes) |scene| {
+        d.frames_set.put(allocator, scene, {}) catch unreachable;
+    }
+
     const data: *ReadScenesData = allocator.create(ReadScenesData) catch unreachable;
     data.* = d;
 
@@ -86,17 +122,9 @@ export fn readScenesCreate(in: ?*const vs.Map, out: ?*vs.Map, user_data: ?*anyop
         },
     };
 
-    // const getFrame = switch (d.vi.format.bytesPerSample) {
-    //     1 => &ReadScenes(u8).getFrame,
-    //     2 => if (d.vi.format.sampleType == vs.SampleType.Integer) &ReadScenes(u16).getFrame else &ReadScenes(f16).getFrame,
-    //     4 => &ReadScenes(f32).getFrame,
-    //     else => unreachable,
-    // };
-    // const getFrame = getFrame;
-
     vsapi.?.createVideoFilter.?(out, "ReadScenes", d.vi, getFrame, readScenesFree, fm.Parallel, &deps, deps.len, data, core);
 }
 
 pub fn registerFunction(plugin: *vs.Plugin, vsapi: *const vs.PLUGINAPI) void {
-    _ = vsapi.registerFunction.?("ReadScenes", "clip:vnode;mode:int[]", "clip:vnode;", readScenesCreate, null, plugin);
+    _ = vsapi.registerFunction.?("ReadScenes", "clip:vnode;path:data", "clip:vnode;", readScenesCreate, null, plugin);
 }
