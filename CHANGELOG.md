0df125ae608f3dcfe6155df3782449865b3bfeeb
Austin Dworaczyk Wiltshire <561689+adworacz@users.noreply.github.com>
2025-06-02 13:07:56 -0700

    Bump to version 0.2, add CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## 0.2 - TBD
+
+## 0.1
+* Initial release
+* Add `ReadScenes` function.
+* Support [av-scenechange](https://github.com/rust-av/av-scenechange) scenes file.
--- src/version.zig
+++ src/version.zig
@@ -1,4 +1,4 @@
 const std = @import("std");
 
 // Expose a version that's easily consumable throughout the project.
-pub const version = std.SemanticVersion{ .major = 0, .minor = 1, .patch = 0 };
+pub const version = std.SemanticVersion{ .major = 0, .minor = 2, .patch = 0 };
